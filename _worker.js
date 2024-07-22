f94af037a739482e525d4423660eda8e6e3e9277
huu3323 <143768334+huu3323@users.noreply.github.com>
2024-07-22 22:04:39 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -22,7 +22,7 @@ let addressesnotls = [
 	'www.who.int:8880#官方优选域名',
 ];
 
- 设置优选noTLS地址api接口
+//  设置优选noTLS地址api接口
 let addressesnotlsapi = [
 	'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 ];
